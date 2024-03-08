f7989170d0bd0bedb3e7e73a55764a19bc3ec93a
Wouter Steggerda <woutersteggerda@gmail.com>
2024-03-08 12:37:24 +0100

    1478 - change user observer so it allows a softdeletable user. It will only delete the connected manager on a hard delete

--- src/Observers/UserObserver.php
+++ src/Observers/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace Vng\DennisCore\Observers;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\App;
 use Vng\DennisCore\Interfaces\DennisUserInterface;
 use Vng\DennisCore\Interfaces\IsManagerInterface;
@@ -31,12 +32,25 @@ class UserObserver
         }
     }
 
-    public function deleted(IsManagerInterface $user): void
+    public function deleted(IsManagerInterface $user)
+    {
+        if (!in_array(SoftDeletes::class, class_uses_recursive(get_class($user)))) {
+            // No softdeletes used, hard delete user
+            $this->hardDelete($user);
+        }
+    }
+
+    public function forceDeleted(IsManagerInterface $user)
+    {
+        $this->hardDelete($user);
+    }
+
+    protected function hardDelete(IsManagerInterface $user)
     {
         $manager = $user->getManager();
         if (!is_null($manager)) {
             $this->managerRepository->delete($manager->id);
         }
-
     }
+
 }
